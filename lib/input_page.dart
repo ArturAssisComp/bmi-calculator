48984d9cbce9e18cb90d60b450d038b82eb5c10b
Artur Assis Alves <60040236+ArturAssisComp@users.noreply.github.com>
2023-04-05 11:51:31 -0300

    Add custom theme and refactor the code to separate each page to a different dart file

---
+++ lib/input_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text(
+          'Body Text',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/themes.dart
@@ -0,0 +1,9 @@
+//Custom themes:
+import 'package:flutter/material.dart';
+
+final ThemeData mainTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: const Color(0xFF0D101F),
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: const Color(0xFF0F1224),
+  ),
+);
